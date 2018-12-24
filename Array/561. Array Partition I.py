ce78b8a594f7582213796730f0b6ceb5df42860b
snow <32005018+SnowColdplay@users.noreply.github.com>
2018-12-24 11:18:02 +0800

    Add files via upload

---
+++ Array/561. Array Partition I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        res=0
+        i=0
+        while i<len(nums):
+            res+=nums[i]
+            i+=2
+        return res
